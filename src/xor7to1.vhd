b8b0d09e9c3f7d3cf8042ebe007b24eb99bde725
Gabriele Suma <g.suma@studenti.unipi.it>
2024-01-19 10:42:37 +0100

    made xorNto1 and commented and slightly modified tb1

--- src/xor7to1.vhd
+++ src/xor7to1.vhd
@@ -2,13 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity seven_input_xor is
+    generic(
+        N : integer := 7
+    );
     port(
-        xor_input : in std_logic_vector(6 downto 0);
+        xor_input : in std_logic_vector(N-1 downto 0);
         y : out std_logic
     );
 end entity seven_input_xor;
 
 architecture xor_arch of seven_input_xor is
 begin
-    y <= xor_input(0) xor xor_input(1) xor xor_input(2) xor xor_input(3) xor xor_input(4) xor xor_input(5) xor xor_input(6);
+    p: process(xor_input)
+    variable xor_result : std_logic;
+    begin
+        xor_result := xor_input(0);
+        for i in 1 to N-1 loop
+            xor_result := xor_result xor xor_input(i);
+        end loop;
+    y <= xor_result;
+    end process;
 end architecture;
--- tb/uart_tb1.vhd
+++ tb/uart_tb1.vhd
@@ -33,16 +33,16 @@ architecture test1 of uart_tb1 is
             wait for clk_period*clk_per_bit;
         
             -- Simulating two words sent respecting timing constraints. At the end it will be driven a reset.
-            x_valid <= '1';
-            x <= "1000000";
-            wait for clk_period;
+            x_valid <= '1'; -- Correctly driving both inputs in order to test the correct behaviour of the circuit.
+            x <= "1000000"; 
+            wait for clk_period; -- Driving correctly x_valid to start the transmission.
             x_valid <= '0';
-            wait for clk_period*clk_per_bit*12;
+            wait for clk_period*clk_per_bit*12; -- Here the circuit has just finished the transmission of the first word and it's going to start the transmission of the second one.
             x_valid <= '1';
             x <= "1111111";
             wait for clk_period;
             x_valid <= '0';
-            wait for clk_period*clk_per_bit*12;
+            wait for clk_period*clk_per_bit*13; --In this case it's going to wait another cycle in order to test if the circuit drives tx to '1' when the latter is idle.
             resetn_i <= '1';
             wait for clk_period;
             testing <= false;
